1d1f8ccc673fc816e18c9ca99bc9f2961aef5e14
Marchjul <marchellogiovani28@gmail.com>
2026-03-03 15:52:29 +0700

    Add files via upload

---
+++ Praktikum2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi global
+    float p,l;
+ 
+//implementasi
+void input(){
+    cout << "Masukan Panjang : ";
+    cin >> p;
+    cout << "Masukan Lebar : " ;
+    cin >> l;
+}
+
+float luaspersegi(float x, float y){
+    return x*y;
+}
+int jumlah(int a, int b, int c){
+    return a+b+c;
+}
+
+void output (){
+    cout << "Hasilnya = " << luaspersegi(p,l)<< endl;
+}
+
+int main()
+{
+    input();
+    output();
+    cout << "Hasil Penjumlahan = " << jumlah(5,6,9);
+}//selesai
